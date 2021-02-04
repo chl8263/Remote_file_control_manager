2b6f352c1be58ad57da24fef1051b3fd516bdc3d
Ewan-desktop <chl8263@naver.com>
2021-02-04 22:14:07 +0900

    init

---
+++ rfcm_server/src/test/java/com/ewan/rfcm/RemoteFileControlManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ewan.rfcm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteFileControlManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
