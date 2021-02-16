d2c19fcf38ce65c5f72755a4de2b6a4054846590
Ewan-desktop <chl8263@naver.com>
2021-02-16 23:58:50 +0900

    [server] Create MessageProtocol

---
+++ rfcm_server/src/main/java/com/ewan/rfcm/server/protocol/MessageProtocol.java
@@ -0,0 +1,8 @@
+package com.ewan.rfcm.server.protocol;
+
+public class MessageProtocol {
+    public static final byte CHAT = 0;
+    public static final byte BATTLE_START = 1;
+    public static final byte BATTLE_END = 2;
+    public static final byte WHISPHER = 3;
+}
